1b9c42f1e6614e2bf7dba95d5a4550bad1f986aa
Mohamed Mattar <UG_31093825@f-eng.tanta.edu.eg>
2024-09-16 12:21:23 +0300

    Added task1

---
+++ Task 1/Program.cs
@@ -0,0 +1,102 @@
+﻿namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q1
+            //Console.WriteLine("Mohamed Mattar");
+            //Console.WriteLine("Age : 21");
+            //Console.WriteLine("FavHobbie");
+            #endregion
+
+            #region Q2
+            //int x = 8;
+            //double y = 16.7d;
+            //string s= "Mohamed Mattar";
+            //bool b = true;
+            //Console.WriteLine($"Integer is {x}");
+            //Console.WriteLine($"Double is {y}");
+            //Console.WriteLine($"String is {s}");
+            //Console.WriteLine($"Boolean is {b}");
+            #endregion
+
+            #region Q3
+            //float c = float.Parse(Console.ReadLine());
+            //float f = (c * 9 / 5) + 32;
+            //Console.WriteLine($"Temperature in Celsius is {c}");
+            //Console.WriteLine($"Temperature in Fahrenheit is {f}");
+            #endregion
+
+            #region Q4
+            //float n1=float.Parse(Console.ReadLine());
+            //float n2=float.Parse(Console.ReadLine());
+            //Console.WriteLine($"Sum is {n1+n2}");
+            //Console.WriteLine($"Sub is {n1-n2}");
+            //Console.WriteLine($"Mul is {n1*n2}");
+            //Console.WriteLine($"Div is {n1/n2}");
+            #endregion
+
+            #region Q5
+            //Console.WriteLine("Enter the length, then width:");
+            //float l = float.Parse(Console.ReadLine());
+            //float w = float.Parse(Console.ReadLine());
+            //Console.WriteLine($"Area is {l * w}");
+            #endregion
+
+            #region Q6
+            //int x=int.Parse(Console.ReadLine());
+            //if (x % 2 == 0)
+            //{
+            //    Console.WriteLine("Even");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Odd");
+            //}   
+            #endregion
+
+            #region Q7
+            //Console.WriteLine("Enter your age:");
+            //int age=int.Parse(Console.ReadLine());
+            //if(age >= 18)
+            //{
+            //    Console.WriteLine("You are eligible to vote");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("You are not eligible to vote");
+            //}
+            #endregion
+
+            #region Q8
+            //Console.WriteLine("Enter your grade:");
+            //int grade = int.Parse(Console.ReadLine());
+            //if (grade > 100)
+            //{
+            //    Console.WriteLine("Enter valid grade");
+            //}
+            //else if (grade >= 90)
+            //{
+            //    Console.WriteLine("A");
+            //}
+            //else if (grade >= 80)
+            //{
+            //    Console.WriteLine("B");
+            //}
+            //else if (grade >= 70)
+            //{
+            //    Console.WriteLine("C");
+            //}
+            //else if (grade >= 60)
+            //{
+            //    Console.WriteLine("D");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("F");
+            //}
+            #endregion
+        }
+    }
+}
